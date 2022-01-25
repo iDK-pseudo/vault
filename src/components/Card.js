15511d5a862ab53a6b8e38076063d5014fea116e
iDK-pseudo <shivamsharma151999@gmail.com>
2022-01-25 23:18:31 +0530

    Dynamic rendering of Bank Logos, Card Font style update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,40 @@
 import { useState, useEffect } from 'react';
-import SBILogo from '../static/SBI-logo.svg';
 import '../styles/Card.css';
 
 function Card (props) {
     
     const [card, setCard] = useState({});
+    const [logo, setLogo] = useState(null);
 
-    useEffect(()=>{
+    useEffect( async ()=>{
         const card = {};
+        let useLogo = "", logoImg = "";
         if(props.selectedCard){
             card.cardnum1 = props.selectedCard.cardnum.slice(0,4);
             card.cardnum2 = props.selectedCard.cardnum.slice(4,8);
             card.cardnum3 = props.selectedCard.cardnum.slice(8,12);
             card.cardnum4 = props.selectedCard.cardnum.slice(12,16);
             card.expires = props.selectedCard.expires.slice(5,7)+"/"+props.selectedCard.expires.slice(2,4);
+            if(props.selectedCard.bank.includes("IDBI")){
+                useLogo = await import('../static/IDBI Logo.png');
+                logoImg = <img className="logo" src={useLogo.default}/>
+            }else if(props.selectedCard.bank.includes("HDFC")){
+                useLogo = await import('../static/HDFC Logo.jpg');
+                logoImg = <img className="logo" src={useLogo.default}/>
+            }else if(props.selectedCard.bank.includes("SBI")){
+                useLogo = await import('../static/SBI Logo.png');
+                logoImg = <img className="logo" src={useLogo.default}/>
+            }
         }
         setCard(card);
+        setLogo(logoImg);
     },[props.selectedCard]);
 
     return (
         <div className="card">
-            <img className="bank-logo" src={SBILogo}/>
+            <div className="logo-wrapper">
+                {logo}
+            </div>
             <div className="card-number">
                 <div>
                     {card.cardnum1}
