427a7efc993c5673c88529e4fe7b0d6dc12c8164
iDK-pseudo <shivamsharma151999@gmail.com>
2022-02-10 22:30:21 +0530

    Added individual states for form fields

--- src/components/mui_components/AddCardDrawer.js
+++ src/components/mui_components/AddCardDrawer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Drawer from '@mui/material/Drawer';
 import Input from '@mui/material/Input';
 import InputLabel from '@mui/material/InputLabel';
@@ -10,86 +10,128 @@ import Button from '@mui/material/Button';
 import AddCardIcon from '@mui/icons-material/AddCard';
 
 export default function AddCardDrawer(props) {
-  return (
-    <Drawer 
-        open={props.open} 
-        anchor="bottom" 
-        PaperProps = {{sx: {padding: 5}}}
-        onClose={props.handleDrawerClose}
-    >   
-        <InputLabel>
-                Card Number
-        </InputLabel>
-        <Input
-            startAdornment={
-                <InputAdornment position="start">
-                    <CreditCardIcon fontSize="small"/>
-                </InputAdornment>
-            }
-            placeholder="1234 2232 1232 1223"
-            sx = {{
-                fontSize: '25px',
-                fontWeight: 'bolder'
-            }}
-        />
-        <div style= {{display: 'flex', marginTop: '30px'}}>
-            <div>
+    const [cardnum, setCardNum] = useState("");
+    const [month, setMonth] = useState("");
+    const [year, setYear] = useState("");
+    const [cvv, setCVV] = useState("");
+
+    const handleEntry = (e) => {
+        switch(e.target.name){
+            case 'cardnum': 
+                if(e.nativeEvent.inputType.includes("insert") && cardnum.length<16) setCardNum(e.target.value); 
+                else if(e.nativeEvent.inputType.includes("delete") && cardnum.length>0) setCardNum(e.target.value);
+                break;
+            case 'month': 
+                if(e.nativeEvent.inputType.includes("insert") && month.length<2) setMonth(e.target.value); 
+                else if(e.nativeEvent.inputType.includes("delete") && month.length>0) setMonth(e.target.value);
+                break;
+            case 'year':
+                if(e.nativeEvent.inputType.includes("insert") && year.length<4) setYear(e.target.value); 
+                else if(e.nativeEvent.inputType.includes("delete") && year.length>0) setYear(e.target.value);
+                break;
+            case 'cvv':
+                if(e.nativeEvent.inputType.includes("insert") && cvv.length<3) setCVV(e.target.value); 
+                else if(e.nativeEvent.inputType.includes("delete") && cvv.length>0) setCVV(e.target.value);
+                break;
+        }
+    }
+
+    return (
+        <Drawer 
+            open={props.open} 
+            anchor="bottom"
+            PaperProps = {{sx: {padding: 5}}}
+            onClose={props.handleDrawerClose}
+        >   
             <InputLabel>
-                    Month
+                    Card Number
             </InputLabel>
             <Input
+                name="cardnum"
+                type="number"
+                value={cardnum}
+                onChange={handleEntry}
                 startAdornment={
                     <InputAdornment position="start">
-                        <EventIcon fontSize="small"/>
+                        <CreditCardIcon fontSize="small"/>
                     </InputAdornment>
                 }
-                placeholder="01"
+                placeholder="1234 2232 1232 1223"
                 sx = {{
-                    fontSize: '20px',
-                    marginRight: '20px',
+                    fontSize: '25px',
                     fontWeight: 'bolder'
                 }}
             />
+            <div style= {{display: 'flex', marginTop: '30px'}}>
+                <div>
+                <InputLabel>
+                        Month
+                </InputLabel>
+                <Input
+                    name="month"
+                    type="number"
+                    value={month}
+                    onChange={handleEntry}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <EventIcon fontSize="small"/>
+                        </InputAdornment>
+                    }
+                    placeholder="01"
+                    sx = {{
+                        fontSize: '20px',
+                        marginRight: '20px',
+                        fontWeight: 'bolder'
+                    }}
+                />
+                </div>
+                <div>
+                <InputLabel>
+                        Year
+                </InputLabel>
+                <Input
+                    name="year"
+                    type="number"
+                    value={year}
+                    onChange={handleEntry}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <EventIcon fontSize="small"/>
+                        </InputAdornment>
+                    }
+                    placeholder="2020"
+                    sx = {{
+                        fontSize: '20px',
+                        marginRight: '20px',
+                        fontWeight: 'bolder'
+                    }}
+                />
+                </div>
+                <div>
+                <InputLabel>
+                        CVV
+                </InputLabel>
+                <Input
+                    name="cvv"
+                    type="number"
+                    value={cvv}
+                    onChange={handleEntry}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <LockIcon fontSize="small"/>
+                        </InputAdornment>
+                    }
+                    placeholder="123"
+                    sx = {{
+                        fontSize: '20px',
+                        fontWeight: 'bolder'
+                    }}
+                />
+                </div>
             </div>
-            <div>
-            <InputLabel>
-                    Year
-            </InputLabel>
-            <Input
-                startAdornment={
-                    <InputAdornment position="start">
-                        <EventIcon fontSize="small"/>
-                    </InputAdornment>
-                }
-                placeholder="2020"
-                sx = {{
-                    fontSize: '20px',
-                    marginRight: '20px',
-                    fontWeight: 'bolder'
-                }}
-            />
-            </div>
-            <div>
-            <InputLabel>
-                    CVV
-            </InputLabel>
-            <Input
-                startAdornment={
-                    <InputAdornment position="start">
-                        <LockIcon fontSize="small"/>
-                    </InputAdornment>
-                }
-                placeholder="123"
-                sx = {{
-                    fontSize: '20px',
-                    fontWeight: 'bolder'
-                }}
-            />
-            </div>
-        </div>
-        <Button variant="contained" size="large" sx={{marginTop: "50px"}} startIcon={<AddCardIcon/>}>
-            Add
-        </Button>
-    </Drawer>
-  )
+            <Button variant="contained" size="large" sx={{marginTop: "50px"}} startIcon={<AddCardIcon/>} onClick={props.handleAddNewCard}>
+                Add
+            </Button>
+        </Drawer>
+    )
 }
