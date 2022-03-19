ed6166a573c0b9173f19751767d8d44725ded610
iDK-pseudo <shivamsharma151999@gmail.com>
2022-03-19 13:57:32 +0530

    Bottom Nav add button update

--- src/components/mui_components/BottomNav.js
+++ src/components/mui_components/BottomNav.js
@@ -1,9 +1,11 @@
 import React from "react";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Button from "@mui/material/Button";
 import HomeIcon from "@mui/icons-material/Home";
 import AddCardIcon from "@mui/icons-material/AddCard";
 import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
 
 export default function BottomNav(props) {
     const [value, setValue] = React.useState(0);
@@ -16,7 +18,8 @@ export default function BottomNav(props) {
             sx={{
                 position: "absolute",
                 bottom: 0,
-                width: "90%",
+                left: "10%",
+                width: "80%",
                 background: "#ECEDF3",
                 borderRadius: 4,
             }}
@@ -24,42 +27,54 @@ export default function BottomNav(props) {
             <BottomNavigationAction
                 sx={{
                     "&.MuiBottomNavigationAction-root *": {
-                        color: selected === "home" ? "white" : "#3D4A5C",
+                        color: "#3D4A5C",
                     },
-                    backgroundColor: selected === "home" ? "#98a7bb" : "",
                     borderRadius: 4,
                 }}
-                label="Home"
-                icon={<HomeIcon fontSize="medium" />}
+                icon={
+                    <HomeIcon
+                        fontSize="large"
+                        style={{
+                            borderBottom:
+                                selected === "home" ? "4px solid black" : "",
+                        }}
+                    />
+                }
                 onClick={(e) => {
                     setSelected("home");
                 }}
             />
+            <BottomNavigationAction onClick={props.handleAddCard} />
+            <Button
+                onClick={props.handleAddCard}
+                sx={{
+                    position: "absolute",
+                    bottom: 10,
+                    height: 60,
+                    background: "#F2A313",
+                    borderRadius: 10,
+                }}
+            >
+                <AddIcon fontSize="large" style={{ color: "white" }} />
+            </Button>
             <BottomNavigationAction
                 sx={{
                     "&.MuiBottomNavigationAction-root *": {
-                        color: selected === "add" ? "white" : "#3D4A5C",
+                        color: "#3D4A5C",
                     },
-                    backgroundColor: selected === "add" ? "#98a7bb" : "",
                     borderRadius: 4,
                 }}
-                label="Add"
-                icon={<AddCardIcon fontSize="medium" />}
-                onClick={(e) => {
-                    setSelected("add");
-                    props.handleAddCard(e);
-                }}
-            />
-            <BottomNavigationAction
-                sx={{
-                    "&.MuiBottomNavigationAction-root *": {
-                        color: selected === "settings" ? "white" : "#3D4A5C",
-                    },
-                    backgroundColor: selected === "settings" ? "#98a7bb" : "",
-                    borderRadius: 4,
-                }}
-                label="Settings"
-                icon={<SettingsIcon fontSize="medium" />}
+                icon={
+                    <SettingsIcon
+                        fontSize="large"
+                        style={{
+                            borderBottom:
+                                selected === "settings"
+                                    ? "4px solid black"
+                                    : "",
+                        }}
+                    />
+                }
                 onClick={(e) => {
                     setSelected("settings");
                 }}
